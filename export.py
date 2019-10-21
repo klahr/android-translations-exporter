9873340af9ae29831d8f4fda5adaf16da9e6ef1e
Joachim Klahr <joachim.klahr@gmail.com>
2019-10-21 14:38:13 +0200

    First commit.

---
+++ export.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+from xml.dom import minidom
+
+def isTranslated(translations, key):
+	for translation in translations:
+		elements = translation["xmldoc"].getElementsByTagName("string")
+		translated = False
+		for element in elements:
+			if element.attributes["name"].value == key:
+				if element.firstChild != None:
+					translated = True
+					break
+		
+		if translated == False:
+			return False
+
+	return True
+
+def getTranslation(translations, key, language):
+	for translation in translations:
+		if translation["language"] == language:
+			elements = translation["xmldoc"].getElementsByTagName("string")
+			for element in elements:
+				if element.attributes["name"].value == key:
+					if element.firstChild is not None:
+						return element.firstChild.data
+					else:
+						return ""
+
+	return None
+
+def getFilenameForLanguage(filename, language):
+	return filename[0:filename.rfind('/')] + "-" + language + "/strings.xml"
+
+def doExport(filename, languages):
+	translations = []
+	for language in languages:
+		stringsFilename = getFilenameForLanguage(filename, language)
+		if os.path.isfile(stringsFilename):
+			xmldoc = minidom.parse(stringsFilename)
+			elements = xmldoc.getElementsByTagName("string")
+			dictionary = {}
+			dictionary["language"] = language
+			dictionary["filename"] = stringsFilename
+			dictionary["xmldoc"] = xmldoc
+			translations.append(dictionary)
+
+	file = open("out.csv", "w")
+
+	xmldoc = minidom.parse(filename)
+	stringElements = xmldoc.getElementsByTagName("string")
+	file.write("key;default;")
+	for translation in translations:
+		file.write(translation["language"] + ";")
+
+	file.write("\n")
+
+	for key in stringElements:
+		if key.hasAttribute("translatable"):
+			if key.attributes["translatable"].value == "false":
+				continue
+
+		if not isTranslated(translations, key.attributes["name"].value):
+			file.write(key.attributes["name"].value + ";")
+			file.write(key.firstChild.data + ";")
+			for translation in translations:
+				t = getTranslation(translations, key.attributes["name"].value, translation["language"])
+				if t is not None:
+					file.write(t + ";")
+				else:
+					file.write(";")
+
+			file.write("\n")
+
+	file.close()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--filename", action="store")
+parser.add_argument("--languages", action="store")
+results = parser.parse_args()
+
+if "filename" not in results or results.filename is None:
+	print("usage: ./export.py --filename </PATH/TO/strings.xml> --languages <COMMA_SEPARATED_LANGUAGES>")
+	print("example: ./export.py --filename ./app/src/main/res/values/strings.xml --languages=sv,no,fi")
+else:
+	languages = results.languages.split(',')
+	doExport(results.filename, languages)
